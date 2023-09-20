69a412b6165b1fad5e76558351f7a4ac88411342
nadiaS11 <nadiasultana.ns11@gmail.com>
2023-09-21 01:49:53 +0600

    nav made sticky

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -3,24 +3,27 @@ import { NavLink, Outlet } from "react-router-dom";
 const MainLayout = () => {
   return (
     <div>
-      <div className="flex flex-col md:flex-row text-center items-center justify-between ">
-        <h1 className=" text-xl font-bold py-2">Meals Depot</h1>
-        <nav className="flex flex-col md:flex-row gap-5 text-md font-semibold ">
+      <div className="bg-white flex flex-col md:flex-row  justify-between md:py-3 container mx-auto items-center dark:bg-gray-900 fixed z-20 top-0  border-b border-gray-200 dark:border-gray-600 ">
+        <h1 className=" self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+          Meals Depot
+        </h1>
+
+        <nav className=" flex  gap-2 flex-row md:gap-5 text-md font-semibold py-2">
           <NavLink
-            className={`text-gray-500 hover:bg-blue-700 hover:text-white font-bold py-1 px-4 rounded`}
+            className={`hover:text-white  hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 `}
             to="/"
           >
             Home
           </NavLink>
           <NavLink
-            className={`hover:bg-blue-700 text-gray-500 hover:text-white font-bold px-4  py-1 rounded `}
+            className={`hover:text-white  hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 `}
             to="/categories"
           >
             Categories
           </NavLink>
           <NavLink
             to="/send-a-meal"
-            className={`hover:bg-blue-700 text-gray-500 hover:text-white font-bold  py-1  px-4 rounded `}
+            className=" hover:text-white  border border-solid border-blue-800  hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Send A Meal
           </NavLink>
